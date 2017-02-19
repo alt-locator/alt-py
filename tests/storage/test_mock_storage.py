100549ab91015aa1a2f46e81bce04cbb8c64bd28
Craig Nishina <craig.nishina@gmail.com>
2017-02-19 02:23:04 -0800

    chore(mock): implement mock storage
    - update unit tests

--- tests/storage/test_mock_storage.py
+++ tests/storage/test_mock_storage.py
@@ -1,4 +1,5 @@
 from context import alt_py
+from alt_py import storage
 
 import unittest
 
@@ -7,21 +8,34 @@ class TestMockStorage(unittest.TestCase):
     """Mock storage tests"""
 
     def test_get_hosts(self):
-        mockStorage = alt_py.storage.mock_storage.MockStorage()
-        getHosts = mockStorage.get_hosts()
-        self.assertEqual(getHosts, ['get hosts'])
+        mock_storage = storage.mock_storage.MockStorage()
+        location = storage.location.Location()
+        location.name = 'foobar'
+        mock_storage.hosts.append(location)
+
+        get_hosts = mock_storage.get_hosts()
+        self.assertEqual(len(get_hosts), 1)
+        self.assertEqual(get_hosts[0].name, 'foobar')
 
     def test_update_host(self):
-        mockStorage = alt_py.storage.mock_storage.MockStorage()
-        location = alt_py.storage.location.Location()
-        updateHost = mockStorage.update_host(location)
-        self.assertTrue(updateHost)
+        mock_storage = storage.mock_storage.MockStorage()
+        location = storage.location.Location()
+        location.name = 'foobar'
+
+        update_host = mock_storage.update_host(location)
+        self.assertTrue(update_host)
+        self.assertEqual(len(mock_storage.hosts), 1)
+        self.assertEqual(mock_storage.hosts[0].name, 'foobar')
 
     def test_remove_host(self):
-        mockStorage = alt_py.storage.mock_storage.MockStorage()
-        location = alt_py.storage.location.Location()
-        removeHost = mockStorage.remove_host(location)
-        self.assertTrue(removeHost)
+        mock_storage = storage.mock_storage.MockStorage()
+        location = storage.location.Location()
+        location.name = 'foobar'
+        mock_storage.hosts.append(location)
+
+        remove_host = mock_storage.remove_host(location)
+        self.assertTrue(remove_host)
+        self.assertEqual(len(mock_storage.hosts), 0)
 
 
 if __name__ == '__main__':
